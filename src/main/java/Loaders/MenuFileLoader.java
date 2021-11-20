e97addda599534de3a111603a5604619142f8297
YourLord00 <75454768+YourLord00@users.noreply.github.com>
2021-11-20 15:08:31 -0500

    done MenuFileLoader

--- src/main/java/Loaders/MenuFileLoader.java
+++ src/main/java/Loaders/MenuFileLoader.java
@@ -14,6 +14,7 @@ public class MenuFileLoader {
 
     private final static String[] HEADERS = {"MenuItem Name", "Type" ,"Price", "Calorie"};
     private String fileName;
+    private final int numOfCalories = 3;
     public MenuFileLoader(String fileName){
         this.fileName = fileName;
     }
@@ -27,32 +28,25 @@ public class MenuFileLoader {
         List<MenuItem> menuItems = new ArrayList<MenuItem>();
         int numberOfColumns = HEADERS.length;
         boolean useDefault = true;
-
-        BufferedReader breader = null;File file;
+        BufferedReader breader = null;
+        File file = new File(fileName);
         List<List<String>> result = new ArrayList<List<String>>();
-
-
         try {
-            file = new File(fileName);
             if (!file.exists()) {
                 if(useDefault){
                     file = getDefaultFile();
-                }else
-                    throw new IllegalArgumentException("The specified Components.Menu File does not exist.");
+                }else {
+                    throw new IllegalArgumentException(
+                            "The specified Components.Menu File does not exist.");
+                }
             }
-
             breader = new BufferedReader(new InputStreamReader(new FileInputStream(file), "UTF-8"));
-
-
             String line = breader.readLine();
             while(line != null){
                 String[] entries = line.split(",");
                 List<String> lineEntry = new ArrayList<String>();
-
                 if(entries.length != numberOfColumns){
                     throw new IllegalArgumentException("The specified Columns are incorrect.");
-                }else{
-
                 }
                 for(String entry : entries){
                     lineEntry.add(entry);
@@ -60,23 +54,19 @@ public class MenuFileLoader {
                 result.add(lineEntry);
                 line = breader.readLine();
             }
-        } catch (IOException e) {
-
-        } finally {
-            try {
-                if (breader != null)
-                    breader.close();
-
-
-            } catch (IOException ex){
-                ex.printStackTrace();
-            }
+            breader.close();
         }
+         catch (IOException ex) {ex.printStackTrace();}
+        finally{ return getMenuItems(result,menuItems);}
+    }
+    public List<MenuItem> getMenuItems(List<List<String>> result,List<MenuItem> menuItems){
         for(List<String> line : result ){
-             String dishName = line.get(0).trim();String dishType = line.get(1).toUpperCase().trim();
-             double dishPrice = Double.parseDouble(line.get(2).trim());double dishCalorie = Double.parseDouble(line.get(3).trim());
-             MenuItem menuItem = new MenuItem(dishName,dishType,dishPrice,dishCalorie);
-             menuItems.add(menuItem);
+            String dishName = line.get(0).trim();
+            String dishType = line.get(1).toUpperCase().trim();
+            double dishPrice = Double.parseDouble(line.get(2).trim());
+            double dishCalorie = Double.parseDouble(line.get(numOfCalories).trim());
+            MenuItem menuItem = new MenuItem(dishName,dishType,dishPrice,dishCalorie);
+            menuItems.add(menuItem);
         }
         return menuItems;
     }
